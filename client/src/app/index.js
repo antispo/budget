74064003c218267438ba6cee172a014b43505ed9
Mihai Vasilache <antispo@gmail.com>
2019-12-17 19:24:18 +0200

    Add/Delete account works now :D

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,60 +1,114 @@
 import React from 'react'
 
-import AccountFrom from './accountForm'
-import AccountsList from './accountsList'
-
 import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+// import Route from ('react-router-dom').Route
  
 import apis from '../api'
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
+    constructor() {
+        super()
         this.state = {
-            budgetId: "5df2468b0fbf700f3df20683",
-            name: "",
-            accounts: [],
-
+            budget: {
+                _id: "5df2468b0fbf700f3df20683",
+                name: "",
+                accounts: [],
+                categories: [],
+                payess: [],
+                transaction: [],
+            },
+            currentAccount: ""
         }
     }
-
-    loadBudget() {
-        apis.getBudgetById(this.state.budgetId).then(apiResponse => {
-            console.log("loadBudget", apiResponse)
-            this.setState({ name: apiResponse.data.data.name})
+    componentDidMount() {
+        apis.getBudgetById(this.state.budget._id).then( apiResponse => {
+            this.setState( prevState => {  
+                prevState.budget.name = apiResponse.data.data.name
+                return { prevState }
+            })
+        }).then( () => {
+            // console.log(this.state.budget.name)
+        })
+        this.gAL()
+    }
+    gAL() {
+        apis.getAllAccounts(this.state.budget._id).then( apiResponse => {
+            this.setState( prevState => {
+                prevState.budget.accounts = apiResponse.data.data
+                return { prevState }
+            })
+        }).then( () => {
+            // console.log(this.state.budget.accounts)
         })
     }
-
-    loadAccounts() {
-        apis.getAllAccounts(this.state.budgetId).then(apiResponse => {
-            console.log("loadAccounts", apiResponse)
-            this.setState({ accounts: apiResponse.data.data })
-        });
+    deleteAccount = (id) => {
+        const newAccounts = this.state.budget.accounts.filter( (value) => {
+            return value._id !== id
+        })
+        apis.deleteAccountById(id)
+        this.setState( prevState => {
+            // console.log(prevState)
+            prevState.budget.accounts = newAccounts
+            return { prevState }
+        })
     }
-
-    componentDidMount() {
-        this.loadBudget()
-        this.loadAccounts()
-        console.log("componentDidMount, App.state:", this.state)
+    addAccount = (e) => {
+        e.preventDefault()
+        const newName = e.target.name.value
+        const newBalance = e.target.balance.value
+        apis.insertAccount({ 
+            budgetId: this.state.budget._id,
+            name: newName,
+            balance: newBalance
+        }).then( () => {
+            this.gAL()
+        })
+        
     }
-
-    saveAccount(name, balance) {
-        console.log(name)
-        const payload = { 
-            budgetId: "5df2468b0fbf700f3df20683", 
-            name: name, 
-            balance: balance }
-        apis.insertAccount(payload)
-    }
-
     render() {
         return (
-            <div id="main">
-                <div>{this.state.name}</div>
-                <h1>Ola, Ioana! :*</h1>
-                <AccountFrom saveAccount={this.saveAccount}/>
-                <AccountsList accounts={this.state.accounts} />
-            </div>
+            <Router>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-5">
+                            <div className="row">
+                                <ul>
+                                    {this.state.budget.accounts.length !== 0 && 
+                                        this.state.budget.accounts.map( (account) => {
+                                            return (
+                                                <li key={account._id} id={account._id}>
+                                                    <span>{account.name}</span> |
+                                                    <span>{account.balance}</span> | 
+                                                    <span
+                                                        onClick={ () => {
+                                                            this.deleteAccount(account._id)
+                                                        }}>
+                                                        del
+                                                    </span>
+                                                </li>
+                                            )
+                                    })}
+                                </ul>
+                            </div>
+                            <div className="form">
+                                <form onSubmit={this.addAccount}>
+                                    <input name="name" type="text" placeholder="Add account" />
+                                    <input name="balance" type="text" placeholder="0" />
+                                    <button>Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                        <div className="col-5">
+                            Transactions
+                            <Route path="/account/:id" exact render={ () => {
+                                return <div>transaction</div>
+                            }}>
+                            </Route>
+                        </div>
+                    </div>
+                </div>
+            </Router>
         )
     }
 }
