c47f42a66710a7e2905e87c4414f99573b5b3cd5
Mihai Vasilache <antispo@gmail.com>
2019-12-19 09:19:00 +0200

    wtf github?

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter as Router} from 'react-router-dom'
 // import Route from ('react-router-dom').Route
- 
+
 import apis from '../api'
 
 const findItemById = (where, id) => {
